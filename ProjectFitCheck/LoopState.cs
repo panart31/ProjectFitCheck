fcbdb07bd542f55dfb23a137cee2e08caefc2361
Panart <artem-panov-2006@bk.ru>
2026-03-07 01:31:40 +0300

    Реализация моделей состояний циклического алгоритма

--- ProjectFitCheck/LoopState.cs
+++ ProjectFitCheck/LoopState.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectFitCheck
+﻿namespace ClassLibraryFitCheck
 {
-    internal class LoopState
+    /// <summary>
+    /// Представляет состояние циклического алгоритма для визуализации и анализа
+    /// </summary>
+    public class LoopState
     {
+        public int[] Array { get; set; } = new int[0];
+        public int CurrentIndex { get; set; }
+        public int Result { get; set; }
+        public int Threshold { get; set; }
+        public bool IsFinished { get; set; }
+        public string Status { get; set; } = "Не инициализирован";
+        public int[] HighlightedIndices => new[] { CurrentIndex };
+        public bool InvariantBeforeStep { get; set; }
+        public bool InvariantAfterStep { get; set; }
+        public int VariantValue { get; set; }
     }
 }
